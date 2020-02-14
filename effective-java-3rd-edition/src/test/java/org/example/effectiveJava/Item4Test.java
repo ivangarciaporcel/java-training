4289cef7f5c36aeaea75d497c3760555f36fd99c
Ivan Garcia <ivan.garcia@vipre.com>
2020-02-14 16:48:43 -0400

    Added noninstantiability reinforcement of a class, by adding a private constructor (item 4).

---
+++ effective-java-3rd-edition/src/test/java/org/example/effectiveJava/Item4Test.java
@@ -0,0 +1,32 @@
+package org.example.effectiveJava;
+
+import org.example.effectiveJava.item4.ItemUtils;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class Item4Test {
+
+    /**
+     * A class can be
+     * made noninstantiable by including a private constructor
+     */
+    @Test
+    public void Get_ListOfNumbers_SumOfNumbers() {
+        int sum = ItemUtils.sumElements(1, 2, 3);
+        assertEquals(6, sum);
+        sum = ItemUtils.sumElements(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+        assertEquals(55, sum);
+    }
+
+    @Test
+    public void Get_ListOfNumbers_Average() {
+        double average = ItemUtils.average(1, 2, 3);
+        assertEquals(2, average, 0.0);
+        average = ItemUtils.average(1, 2, 3, 4);
+        assertEquals(2.5, average, 0.0);
+        Integer[] v = new Integer[]{1, 2};
+        average = ItemUtils.average(v);
+        assertEquals(1.5, average, 0.0);
+    }
+}
